5c394a02bb2d0676736fff2c449724f12045fbec
Lukas Klizas <lukas@kemdu.lt>
2014-04-03 18:47:48 +0100

    added capistrano

---
+++ deploy/config/deploy.rb
@@ -0,0 +1,22 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'BBD'
+set :repo_url, 'https://github.com/nfqakademija/BBD.git'
+
+set :deploy_to, '/home/bbd/public_html'
+set :linked_files, %w{app/config/parameters.yml}
+set :linked_dirs, %w{app/logs app/cache vendor}
+set :keep_releases, 5
+
+namespace :deploy do
+
+  before :publishing, :restart
+
+  before :restart, :clear_cache do
+    on roles(:web) do
+        execute "cd #{release_path} && composer install"
+    end
+  end
+  after :finishing, 'deploy:cleanup'
+end
---
+++ deploy/config/deploy/production.rb
@@ -0,0 +1,2 @@
+
+server 'www.projektai.nfqakademija.lt:22129', user: 'bbd', roles: %w{web}
